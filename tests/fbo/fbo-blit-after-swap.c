bf30fc1d1212e9b2cc3c837271a626b556d4fefb
Federico Mena Quintero <federico@suse.com>
2013-10-17 14:52:31 -0500

    fbo-blit-after-swap: New test for partial blits after a buffer swap
    Supports a "no-swap" command-line argument; see below.
    
    The clutter/cogl libraries try to minimize the area that gets updated on every frame.
    They do this by doing glBlitFramebufferEXT() from the back buffer to the front buffer.
    
    However, this is buggy with software rendering if there has been a
    buffer swap *before* the first blit from the back buffer to the front
    buffer.  In this case, Mesa fails to initialize the real front buffer with the
    contents of the back buffer before doing the blit.
    
    The test does this:
    
    1. Clear the back buffer to blue.
    
    2. Copy the back buffer to the front buffer, by default with a buffer swap (see below).
    
    3. Clear the back buffer to green, and draw a red square in the middle.
    
    4. glBlitFramebuffer() just the red square from the back to the front buffer.
    
    This test supports a "no-swap" command-line argument.  With this
    argument, step (2) will be done via a full-frame glBlitFramebuffer(),
    instead of a buffer swap.  This exposes another bug with hardware rendering, at
    least on Intel graphics.

---
+++ tests/fbo/fbo-blit-after-swap.c
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2013 Suse, Inc. 
+ * Copyright © 2011 Henri Verbeet <hverbeet@gmail.com>
+ * Copyright 2011 Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+/** @file fbo-blit-after-swap.c
+ *
+ * Test a glBlitFrameBuffer() with a smaller-than-the-window region after doing a buffer swap
+ */
+
+#include "piglit-util-gl-common.h"
+
+PIGLIT_GL_TEST_CONFIG_BEGIN
+
+	config.supports_gl_compat_version = 10;
+
+	config.window_visual = PIGLIT_GL_VISUAL_DOUBLE | PIGLIT_GL_VISUAL_RGB;
+	config.requires_displayed_window = true;
+
+PIGLIT_GL_TEST_CONFIG_END
+
+static bool use_swap_buffers = true;
+
+static const GLfloat red[]   = {1.0f, 0.0f, 0.0f, 1.0f};
+static const GLfloat green[] = {0.0f, 1.0f, 0.0f, 1.0f};
+static const GLfloat blue[]  = {0.0f, 0.0f, 1.0f, 1.0f};
+static const GLfloat black[] = {0.0f, 0.0f, 0.0f, 1.0f};
+
+static void
+setup_front_buffer(void)
+{
+	glDrawBuffer(GL_BACK);
+
+	glClearColor(blue[0], blue[1], blue[2], blue[3]);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	if (use_swap_buffers)
+		piglit_swap_buffers();
+	else {
+		glDrawBuffer(GL_FRONT);
+		glReadBuffer(GL_BACK);
+		glBlitFramebufferEXT(0, 0, piglit_width, piglit_height,
+				     0, 0, piglit_width, piglit_height,
+				     GL_COLOR_BUFFER_BIT, GL_NEAREST);
+	}
+}
+
+static void
+setup_back_buffer(void)
+{
+	int w = piglit_width;
+	int h = piglit_height;
+
+	glDrawBuffer(GL_BACK);
+
+	/* Clear to green */
+	glClearColor(green[0], green[1], green[2], green[3]);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	/* Paint a red square in the middle of the back buffer */
+	glColor4fv(red);
+	piglit_draw_rect(w / 4, h / 4, w / 2, h / 2);
+}
+
+static void
+blit_from_back_to_front(void)
+{
+	int w = piglit_width;
+	int h = piglit_height;
+
+	/* Copy just the red square from the back buffer to the blue front buffer */
+	glDrawBuffer(GL_FRONT);
+	glReadBuffer(GL_BACK);
+	glBlitFramebufferEXT(w / 4, h / 4, 3 * w / 4, 3 * h / 4,
+			     w / 4, h / 4, 3 * w / 4, 3 * h / 4,
+			     GL_COLOR_BUFFER_BIT, GL_NEAREST);
+}
+
+enum piglit_result piglit_display(void)
+{
+	int w = piglit_width;
+	int h = piglit_height;
+	bool pass = true;
+
+	piglit_ortho_projection(w, h, GL_FALSE);
+
+	setup_front_buffer();
+	setup_back_buffer();
+
+	blit_from_back_to_front();
+
+	glFlush();
+
+	/* Now see how we did... */
+
+	glReadBuffer(GL_FRONT);
+
+	/* the middle should be red */
+	pass = piglit_probe_pixel_rgb(w / 2, h / 2, red) && pass;
+
+	/* the corners should be blue */
+	pass = piglit_probe_pixel_rgb(0, 0, blue) && pass;
+	pass = piglit_probe_pixel_rgb(w - 1, 0, blue) && pass;
+	pass = piglit_probe_pixel_rgb(0, h - 1, blue) && pass;
+	pass = piglit_probe_pixel_rgb(w - 1, h - 1, blue) && pass;
+
+	return pass ? PIGLIT_PASS : PIGLIT_FAIL;
+}
+
+void piglit_init(int argc, char **argv)
+{
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "no-swap"))
+			use_swap_buffers = false;
+	}
+
+	piglit_require_extension("GL_EXT_framebuffer_object");
+	piglit_require_extension("GL_EXT_framebuffer_blit");
+}
